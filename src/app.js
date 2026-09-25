5d8bface16fbde2ad773764fb1b41c1df18ed386
Janari Altdorf <jaltdorf@tlu.ee>
2026-09-25 14:59:45 +0300

    chore: CORS installed and connected API endpoints to frontend task-tracker

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { getAllTasks, getTaskById } from "./tasks.js";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 // Request logging
