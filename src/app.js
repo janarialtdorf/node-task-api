fc527fc54bfd18952375566cf9053218d588c615
Janari Altdorf <jaltdorf@tlu.ee>
2026-09-25 13:48:53 +0300

    feat: add GET routes with parameter parsing and validation

--- src/app.js
+++ src/app.js
@@ -1,9 +1,48 @@
+// src/app.js
 import express from "express";
+import { getAllTasks, getTaskById } from "./tasks.js";
 
 const app = express();
 
+let tasks = [
+  { id: 1, title: "Õpi Node.js mooduleid", completed: true },
+  { id: 2, title: "Ehita Express API", completed: false },
+  { id: 3, title: "Kirjuta automaattestid", completed: false },
+];
+
 app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "ok" });
+});
+
+//GET /api/tasks?completed=true/false
+app.get("/api/tasks", (req, res) => {
+  const { completed } = req.query;
+
+  if (completed !== undefined) {
+    if (completed !== "true" && completed !== "false") {
+      return res
+        .status(400)
+        .json({ error: "Invalid parameter value for 'completed'" });
+    }
+
+    const isCompleted = completed === "true";
+    const filteredTasks = tasks.filter(
+      (task) => task.completed === isCompleted,
+    );
+    return res.json(filteredTasks);
+  }
+  res.json(getAllTasks(tasks));
+});
+
+// GET /api/tasks/:id
+app.get("/api/tasks/:id", (req, res) => {
+  const task = getTaskById(tasks, req.params.id);
+
+  if (!task) {
+    return res.status(404).json({ error: "Task not found" });
+  }
+
+  res.json(task);
 });
 
 export default app;
